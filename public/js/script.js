11430ebd684e456ed02b75b48e51a40d72034d6e
juliaacampioto <julia.campioto@sptech.school>
2024-03-16 17:52:50 -0300

    testando o carrossel

--- public/js/script.js
+++ public/js/script.js
@@ -3,6 +3,7 @@ const nav = document.querySelector(".nav");
 
 hamburger.addEventListener("click", () => nav.classList.toggle("active"));
 
+// PRIMEIRO CARROSSEL
 
 const cardPremium = document.getElementById("card_premium");
 const cardBest = document.getElementById("card_best");
@@ -16,75 +17,191 @@ let tela = window.innerWidth;
 cardBest.addEventListener("click", () => {
     if (tela <= 710) {
         carrossel.style.marginLeft = "150%";
-        ocultaCard(cardPremium)
-        ocultaCard(cardPro)
-        exporCard(cardBest)
+        ocultaCard(cardPremium);
+        ocultaCard(cardPro);
+        exporCard(cardBest);
     }
-})
-
+});
 
 cardPremium.addEventListener("click", () => {
     if (tela <= 710) {
         carrossel.style.marginRight = "150%";
-        ocultaCard(cardBest)
-        ocultaCard(cardPro)
-        exporCard(cardPremium)
+        ocultaCard(cardBest);
+        ocultaCard(cardPro);
+        exporCard(cardPremium);
     }
-})
-
+});
 
 cardPro.addEventListener("click", () => {
     if (tela <= 710) {
         carrossel.style.marginLeft = "0";
         carrossel.style.marginRight = "0";
-        ocultaCard(cardPremium)
-        ocultaCard(cardBest)
-        exporCard(cardPro)
+        ocultaCard(cardPremium);
+        ocultaCard(cardBest);
+        exporCard(cardPro);
     }
-})
-
+});
 
 const verificaTamanho = () => {
     tela = window.innerWidth;
     if (tela > 710) {
         carrossel.style.marginLeft = "0";
         carrossel.style.marginRight = "0";
-        exporCard(cardPremium)
-        exporCard(cardPro)
-        exporCard(cardBest)
+        exporCard(cardPremium);
+        exporCard(cardPro);
+        exporCard(cardBest);
         qtd = 0;
-    }
-    else {
-        qtd++
-        if (qtd == 1) {
-            ocultaCard(cardPremium)
-            ocultaCard(cardBest)
-            exporCard(cardPro)
+    } else {
+        qtd++;
+        if (qtd === 1) {
+            ocultaCard(cardPremium);
+            ocultaCard(cardBest);
+            exporCard(cardPro);
         }
     }
-}
-
+};
 
 function ocultaCard(card) {
-    card.style.opacity = 0.5
-    card.style.filter = "blur(5px)"
-    card.style.opacity = "350px"
-    card.style.filter = "400px"
-    card.style.scale = "0.9"
-    card.style.cursor = "pointer"
+    card.style.opacity = "0.5";
+    card.style.filter = "blur(5px)";
+    card.style.transform = "scale(0.9)";
+    card.style.cursor = "pointer";
 }
 
 function exporCard(card) {
-    card.style.opacity = 1
-    card.style.filter = "none"
-    card.style.scale = "1"
-    card.style.cursor = ""
+    card.style.opacity = "1";
+    card.style.filter = "none";
+    card.style.transform = "scale(1)";
+    card.style.cursor = "";
 }
 
-
 window.addEventListener("resize", verificaTamanho);
+window.onload = verificaTamanho;
 
-window.onload(verificaTamanho);
+// CARROSSEL NOSSA EQUIPE
 
+const cardGuilherme = document.getElementById("card_guilherme");
+const cardClaudio = document.getElementById("card_claudio");
+const cardDiego = document.getElementById("card_diego");
+const cardJulia = document.getElementById("card_julia");
+const cardMaria = document.getElementById("card_maria");
+const cardThiago = document.getElementById("card_thiago");
+const carrosselEquipe = document.getElementById("id_carrossel_equipe");
 
+cardClaudio.addEventListener("click", () => {
+    carrosselEquipe.style.marginLeft = "117%";
+    carrosselEquipe.style.marginRight = "0";
+        ocultaCardEquipe(cardGuilherme);
+        exporCardEquipe(cardDiego);
+        ocultaCardEquipe(cardJulia);
+        ocultaCardEquipe(cardMaria);
+        ocultaCardEquipe(cardThiago);
+        exporCardEquipe(cardClaudio);
+    
+});
 
+cardGuilherme.addEventListener("click", () => {
+    carrosselEquipe.style.marginLeft = "0";
+    carrosselEquipe.style.marginRight = "0";
+        ocultaCardEquipe(cardClaudio);
+        ocultaCardEquipe(cardDiego);
+        exporCardEquipe(cardJulia);
+        ocultaCardEquipe(cardMaria);
+        ocultaCardEquipe(cardThiago);
+        exporCardEquipe(cardGuilherme);
+    
+});
+
+cardDiego.addEventListener("click", () => {
+        carrosselEquipe.style.marginLeft = "117%";
+        carrosselEquipe.style.marginRight = "0";
+        ocultaCardEquipe(cardGuilherme);
+        exporCardEquipe(cardClaudio);
+        ocultaCardEquipe(cardJulia);
+        ocultaCardEquipe(cardMaria);
+        ocultaCardEquipe(cardThiago);
+        exporCardEquipe(cardDiego);
+    
+});
+
+cardJulia.addEventListener("click", () => {
+    
+        carrosselEquipe.style.marginLeft = "0";
+        carrosselEquipe.style.marginRight = "0";
+        exporCardEquipe(cardGuilherme);
+        ocultaCardEquipe(cardClaudio);
+        ocultaCardEquipe(cardDiego);
+        ocultaCardEquipe(cardMaria);
+        ocultaCardEquipe(cardThiago);
+        exporCardEquipe(cardJulia);
+    
+});
+
+cardMaria.addEventListener("click", () => {
+    
+        carrosselEquipe.style.marginLeft = "0";
+        carrosselEquipe.style.marginRight = "117%";
+        ocultaCardEquipe(cardGuilherme);
+        ocultaCardEquipe(cardClaudio);
+        ocultaCardEquipe(cardDiego);
+        ocultaCardEquipe(cardJulia);
+        exporCardEquipe(cardThiago);
+        exporCardEquipe(cardMaria);
+   
+});
+
+cardThiago.addEventListener("click", () => {
+    
+        carrosselEquipe.style.marginLeft = "0";
+        carrosselEquipe.style.marginRight = "117%";
+        ocultaCardEquipe(cardGuilherme);
+        ocultaCardEquipe(cardClaudio);
+        ocultaCardEquipe(cardDiego);
+        ocultaCardEquipe(cardJulia);
+        exporCardEquipe(cardMaria);
+        exporCardEquipe(cardThiago);
+   
+});
+
+const verificaTamanhoEquipe = () => {
+    tela = window.innerWidth;
+    if (tela > 710) {
+        carrosselEquipe.style.marginLeft = "0";
+        carrosselEquipe.style.marginRight = "0";
+        exporCardEquipe(cardJulia);
+        ocultaCardEquipe(cardMaria);
+        ocultaCardEquipe(cardThiago);
+        exporCardEquipe(cardGuilherme);
+        ocultaCardEquipe(cardDiego);
+        ocultaCardEquipe(cardClaudio);
+
+        qtd = 0;
+    } else {
+        qtd++;
+        if (qtd === 1) {
+            exporCardEquipe(cardGuilherme);
+            ocultaCardEquipe(cardClaudio);
+            exporCardEquipe(cardJulia);
+            ocultaCardEquipe(cardMaria);
+            ocultaCardEquipe(cardThiago);
+            ocultaCardEquipe(cardDiego);
+        }
+    }
+};
+
+function ocultaCardEquipe(card) {
+    card.style.opacity = "0.5";
+    card.style.filter = "blur(5px)";
+    card.style.transform = "scale(0.9)";
+    card.style.cursor = "pointer";
+}
+
+function exporCardEquipe(card) {
+    card.style.opacity = "1";
+    card.style.filter = "none";
+    card.style.transform = "scale(1)";
+    card.style.cursor = "";
+}
+
+window.addEventListener("resize", verificaTamanhoEquipe);
+window.onload = verificaTamanhoEquipe;

5307dce64470e761d472ed402993bea390458071
thiagovieiras <thiago.santos@sptech.school>
2024-03-17 16:36:14 -0300

    tela de cadastro e login arrumado os botões de link

--- public/js/cadastrar.js
+++ public/js/cadastrar.js
@@ -4,57 +4,116 @@ const btPlano = document.getElementById("btPlano");
 
 btPlano.addEventListener("click", mostrarOpcoes); 
 
+const tela = window.innerWidth;
+
 function mostrarOpcoes() {
+    if (tela > 600) {
+
+        const plano1 = document.getElementById("plano1");
+        const plano2 = document.getElementById("plano2");
+        const plano3 = document.getElementById("plano3");
     
-    const plano1 = document.getElementById("plano1");
-    const plano2 = document.getElementById("plano2");
-    const plano3 = document.getElementById("plano3");
-
-    const listaPlanos = [plano1, plano2, plano3];
-
-    plano1.style.top = "100px";
-    plano1.style.visibility = "hidden";
-    plano1.style.opacity = 0;
-
-    plano3.style.top = "100px";
-    plano3.style.visibility = "hidden";
-    plano3.style.opacity = 0;
-
-
-    if (plano2.style.visibility === "visible") {
-        plano3.style.left = "calc(40vw - 15px)"
-        plano3.style.opacity = 0;
+        const listaPlanos = [plano1, plano2, plano3];
+    
+        plano1.style.top = "100px";
+        plano1.style.visibility = "hidden";
+        plano1.style.opacity = 0;
+    
+        plano3.style.top = "100px";
         plano3.style.visibility = "hidden";
-        setTimeout(()=> {
-            plano1.style.left = "calc(40vw - 15px)";
-            plano1.style.opacity = 0;
-            plano1.style.visibility = "hidden";
-        }, 100);
-        setTimeout(()=> {
-            plano2.style.top = "120px"
-            plano2.style.opacity = 0;
-            plano2.style.visibility = "hidden";
-        }, 150);
+        plano3.style.opacity = 0;
+    
+    
+        if (plano2.style.visibility === "visible") {
+            plano3.style.left = "calc(40vw - 15px)"
+            plano3.style.opacity = 0;
+            plano3.style.visibility = "hidden";
+            setTimeout(()=> {
+                plano1.style.left = "calc(40vw - 15px)";
+                plano1.style.opacity = 0;
+                plano1.style.visibility = "hidden";
+            }, 100);
+            setTimeout(()=> {
+                plano2.style.top = "120px"
+                plano2.style.opacity = 0;
+                plano2.style.visibility = "hidden";
+            }, 150);
+        } else {
+            plano2.style.opacity = 1;
+            plano2.style.visibility = "visible";
+            plano2.style.top = "100px"
+            setTimeout(()=> {
+                plano1.style.opacity = 1;
+                plano1.style.visibility = "visible";
+                plano1.style.left = "calc(40vw - 125px)";
+            }, 100);
+            setTimeout(()=> {
+                plano3.style.opacity = 1;
+                plano3.style.visibility = "visible";
+                plano3.style.left = "calc(40vw + 95px)";
+            }, 170);
+    
+            listaPlanos.forEach(plano => {
+                plano.addEventListener("click", () => {
+                    btPlano.innerHTML = plano.innerHTML;
+                })
+            });
+        }
     } else {
-        plano2.style.opacity = 1;
-        plano2.style.visibility = "visible";
-        plano2.style.top = "100px"
-        setTimeout(()=> {
-            plano1.style.opacity = 1;
-            plano1.style.visibility = "visible";
-            plano1.style.left = "calc(40vw - 125px)";
-        }, 100);
-        setTimeout(()=> {
-            plano3.style.opacity = 1;
-            plano3.style.visibility = "visible";
-            plano3.style.left = "calc(40vw + 95px)";
-        }, 170);
-
-        listaPlanos.forEach(plano => {
-            plano.addEventListener("click", () => {
-                btPlano.innerHTML = plano.innerHTML;
-            })
-        });
+        const plano1 = document.getElementById("plano1");
+        const plano2 = document.getElementById("plano2");
+        const plano3 = document.getElementById("plano3");
+    
+        const listaPlanos = [plano1, plano2, plano3];
+    
+        plano1.style.top = "324px";
+        plano1.style.visibility = "hidden";
+        plano1.style.opacity = 0;
+    
+        plano3.style.top = "324px";
+        plano3.style.visibility = "hidden";
+        plano3.style.opacity = 0;
+    
+    
+        if (plano2.style.visibility === "visible") {
+            btPlano.style.visibility = "visible";
+            plano3.style.left = "calc(40vw - 10px)";
+            plano3.style.opacity = 0;
+            plano3.style.visibility = "hidden";
+            setTimeout(()=> {
+                plano1.style.left = "calc(40vw + 20px)";
+                plano1.style.opacity = 0;
+                plano1.style.visibility = "hidden";
+            }, 100);
+            setTimeout(()=> {
+                plano2.style.top = "324px"
+                plano2.style.opacity = 0;
+                plano2.style.visibility = "hidden";
+            }, 150);
+        } else {
+            btPlano.style.visibility = "hidden";
+            plano2.style.opacity = 1;
+            plano2.style.visibility = "visible";
+            plano2.style.top = "324px"
+            setTimeout(()=> {
+                plano1.style.opacity = 1;
+                plano1.style.visibility = "visible";
+                plano1.style.left = "calc(40vw - 105px)";
+            }, 100);
+            setTimeout(()=> {
+                plano3.style.opacity = 1;
+                plano3.style.visibility = "visible";
+                plano3.style.left = "calc(40vw + 125px)";
+            }, 170);
+    
+            listaPlanos.forEach(plano => {
+                plano.addEventListener("click", () => {
+                    btPlano.style.visibility = "visible";
+                    btPlano.innerHTML = plano.innerHTML;
+                    mostrarOpcoes();
+                })
+            });
+        }
     }
 }
 
@@ -108,6 +167,11 @@ function validarCampos() {
                 !new RegExp("^(?=.*\\d)(?=.*[^\\w\\s])(?=.*[A-Z])(?=.*[a-z]).*$").test(input.value)) {
             divMsg.style.display = 'block';
             mensagem.innerHTML = 'A Senha deve conter letras MAIÚSCULAS, minúsculas, números e símbolos'
+            validacaoCampos = false;
+            return;
+        } else if (btPlano.value === "Escolha um plano") {
+            divMsg.style.display = 'block';
+            mensagem.innerHTML = 'Não é possivel cadastrar sem escolher um plano.'
             validacaoCampos = false;
             return;
         }

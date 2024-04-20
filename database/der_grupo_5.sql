7eeffc1449dad88718a628e27ae8c5a52539fdb9
MariaGirote <maria.nascimento@sptech.school>
2024-04-20 16:26:54 -0300

    Atualizei a DER, DER png e fiz script no database

--- database/der_grupo_5.sql
+++ database/der_grupo_5.sql
@@ -2,217 +2,126 @@ CREATE DATABASE der_grupo_5;
 
 USE der_grupo_5;
 
-CREATE TABLE usuario (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(60) NOT NULL,
-    cpf VARCHAR(14),
-    ativo BOOLEAN NOT NULL,
-    email VARCHAR(60) NOT NULL,
-    senha VARCHAR(45) NOT NULL,
-    fkPlano INT NOT NULL,
-        CONSTRAINT fkPlanoUsuario FOREIGN KEY  (fkPlano) REFERENCES plano(id),
-    cargos INT NOT NULL,
-        CONSTRAINT fkCargosUsuario FOREIGN KEY (cargos) REFERENCES cargos(id)
-);
-
 CREATE TABLE cargos (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    idCargos INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(45) NOT NULL
 );
 
-CREATE TABLE identificacao (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    cpf/cnpj VARCHAR(18) NOT NULL,
-    fkUsuario INT NOT NULL,
-        CONSTRAINT fkUsuarioIdentificacao FOREIGN KEY (fkUsuario) REFERENCES usuario(id),
-    fkCargosUsuario INT NOT NULL,
-        CONSTRAINT fkCargosUsuarioIdentificacao FOREIGN KEY (fkCargosUsuario) REFERENCES cargos(id),
-    fkEmpresa INT NOT NULL,
-        CONSTRAINT fkEmpresaIdenficacao FOREIGN KEY (fkEmpresa) REFERENCES empresa(id)
-);
-
-CREATE TABLE empresa (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(50),
-    fkEndereco INT NOT NULL,
-        CONSTRAINT fkEnderecoEmpresa FOREIGN KEY (fkEndereco) REFERENCES endereco(id),
-    
-);
-
-CREATE TABLE endereco (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    cep CHAR(8) NOT NULL,
-    logradouro VARCHAR(60) NOT NULL,
-    numero VARCHAR(4) NOT NULL,
-    bairro VARCHAR(40) NOT NULL,
-    cidade VARCHAR (40) NOT NULL,
-    estado VARCHAR(30) NOT NULL,
-    complemento VARCHAR(45)
-);
-
 CREATE TABLE plano (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    idPlano INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(30) NOT NULL,
     descricao VARCHAR(45) NOT NULL
 );
 
+CREATE TABLE endereco (
+    idEndereco INT PRIMARY KEY AUTO_INCREMENT,
+    cep CHAR(8) NOT NULL,
+    logradouro VARCHAR(60) NOT NULL,
+    numero VARCHAR(4) NOT NULL,
+    bairro VARCHAR(40) NOT NULL,
+    estado VARCHAR(30) NOT NULL,
+    complemento VARCHAR(45)
+);
+
+CREATE TABLE usuario (
+    idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(60) NOT NULL,
+    email VARCHAR(60) NOT NULL,
+    senha VARCHAR(45) NOT NULL,
+    fkPlano INT NOT NULL,
+        CONSTRAINT fkPlanoUsuario FOREIGN KEY (fkPlano) REFERENCES plano(idPlano),
+    fkCargos INT NOT NULL,
+        CONSTRAINT fkCargosUsuario FOREIGN KEY (cargos) REFERENCES cargos(idCargos)
+);
+
+CREATE TABLE identificacao (
+    idIdentificacao INT PRIMARY KEY AUTO_INCREMENT,
+    cpf/cnpj VARCHAR(18) NOT NULL,
+    fkUsuario INT NOT NULL,
+        CONSTRAINT fkUsuarioIdentificacao FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario),
+    fkEmpresa INT NOT NULL,
+        CONSTRAINT fkEmpresaIdenficacao FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
+);
+
+CREATE TABLE empresa (
+    idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50),
+    fkEndereco INT NOT NULL,
+        CONSTRAINT fkEnderecoEmpresa FOREIGN KEY (fkEndereco) REFERENCES endereco(idEndereco),
+    
+);
+
 CREATE TABLE contato (
-    id INT AUTO_INCREMENT,
+    idContato INT AUTO_INCREMENT,
     telefone VARCHAR(10) NOT NULL,
     fkUsuario INT NOT NULL,
-        CONSTRAINT fkUsuarioContato FOREIGN KEY (fkUsuario) REFERENCES usuario(id),
-    PRIMARY KEY (id, fkUsuario)
+        CONSTRAINT fkUsuarioContato FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario),
+    PRIMARY KEY (idContato, fkUsuario)
 );
 
 CREATE TABLE maquina (
-    id INT AUTO_INCREMENT,
+    idMaquina INT AUTO_INCREMENT,
     ipv4 VARCHAR(15) NOT NULL,
     fkUsuario INT NOT NULL,
-        CONSTRAINT fkUsuarioMaquina FOREIGN KEY (fkUsuario) REFERENCES usuario(id),
-    PRIMARY KEY (id, fkUsuario)
+        CONSTRAINT fkUsuarioMaquina FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario),
+    PRIMARY KEY (idMaquina, fkUsuario)
 );
 
-CREATE TABLE placa_mae (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    fabricante VARCHAR(45) NOT NULL,
-    modelo VARCHAR(45) NOT NULL,
-    fkMaquina INT NOT NULL,
-        CONSTRAINT fkMaquinaPlaca_mae FOREIGN KEY (fkMaquina) REFERENCES maquina(id),
-    fkMaquinaUsuario INT NOT NULL,
-        CONSTRAINT fkMaquinaUsuarioPlaca_mae FOREIGN KEY (fkMaquinaUsuario) REFERENCES maquina(fkUsuario),
-    PRIMARY KEY (id, fkMaquina, fkMaquinaUsuario)
-);
-
-CREATE TABLE sistema_operacional (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(45) NOT NULL,
-    versao VARCHAR(45) NOT NULL,
-    usuario VARCHAR(45) NOT NULL,
-    usoCpu DOUBLE(5,2) NOT NULL,
-    usoRam DOUBLE(5,2) NOT NULL
-);
-
-CREATE TABLE relacao_maquina_so (
-    fkMaquina INT NOT NULL,
-        CONSTRAINT fkMaquinaRelacao_maquina_so FOREIGN KEY (fkMaquina) REFERENCES maquina(id),
-    fkMaquinaUsuario INT NOT NULL,
-        CONSTRAINT fkMaquinaUsuarioRelacao_maquina_so FOREIGN KEY (fkMaquinaUsuario) REFERENCES maquina(fkUsuario),
-    fkSistemaOperacional INT NOT NULL,
-        CONSTRAINT fkSistemaOperacionalRelacao_maquina_so FOREIGN KEY (fkSistemaOperacional) REFERENCES sistema_operacional(id),
-    PRIMARY KEY (fkMaquina, fkMaquinaUsuario, fkSistemaOperacional)
-);
-
-CREATE TABLE hdd (
-    id INT, 
-    capacidadeTotal DOUBLE(5,2),
-    numeroParticoes INT NOT NULL,
-    espacoDisponivel VARCHAR(45) NOT NULL,
-    fkMaquina INT NOT NULL,
-        CONSTRAINT fkMaquinaHhd FOREIGN KEY (fkMaquina) REFERENCES maquina(id),
-    fkMaquinaUsuario INT NOT NULL,
-        CONSTRAINT fkMaquinaUsuarioHhd FOREIGN KEY (fkMaquinaUsuario) REFERENCES maquina(fkUsuario),
-    PRIMARY KEY (id, fkMaquina, fkMaquinaUsuario)
-);
-
-CREATE TABLE cpu (
-    id INT,
-    modelo VARCHAR(60) NOT NULL,
-    numeroSerie VARCHAR(50) NOT NULL,
-    fabricante VARCHAR(50) NOT NULL,
-    cache VARCHAR(45),
-    fkMaquina INT NOT NULL,
-        CONSTRAINT fkMaquinaCpu FOREIGN KEY (fkMaquina) REFERENCES maquina(id),
-    fkMaquinaUsuario INT NOT NULL,
-        CONSTRAINT fkMaquinaUsuarioCpu FOREIGN KEY (fkMaquinaUsuario) REFERENCES maquina(fkUsuario),
-    PRIMARY KEY (id, fkMaquina, fkMaquinaUsuario)
-);
-
-CREATE TABLE ram (
-    id INT,
-    capacidadeTotal DOUBLE(5,2) NOT NULL,
-    numeroModulo INT NOT NULL,
-    porcetagemUtilizada DOUBLE(5,2) NOT NULL,
-    fkMaquina INT NOT NULL,
-        CONSTRAINT fkMaquinaRam FOREIGN KEY (fkMaquina) REFERENCES maquina(id),
-    fkMaquinaUsuario INT NOT NULL,
-        CONSTRAINT fkMaquinaUsuarioRam FOREIGN KEY (fkMaquinaUsuario) REFERENCES maquina(fkUsuario),
-    PRIMARY KEY (id, fkMaquina, fkMaquinaUsuario)
-);
-
-CREATE TABLE gpu (
-    id INT AUTO_INCREMENT,
-    modelo VARCHAR(60) NOT NULL,
-    memoria DOUBLE(5,2) NOT NULL,
-    versaoDriver VARCHAR(45) NOT NULL,
-    fkMaquina INT NOT NULL,
-        CONSTRAINT fkMaquinaGpu FOREIGN KEY (fkMaquina) REFERENCES maquina(id),
-    fkMaquinaUsuario INT NOT NULL,
-        CONSTRAINT fkMaquinaUsuarioGpu FOREIGN KEY (fkMaquinaUsuario) REFERENCES maquina(fkUsuario),
-    PRIMARY KEY (id, fkMaquina, fkMaquinaUsuario)    
-);
-
-CREATE TABLE usb (
-    id INT AUTO_INCREMENT,
-    totalPortas INT NOT NULL,
-    fkMaquina INT NOT NULL,
-        CONSTRAINT fkMaquinaUsb FOREIGN KEY (fkMaquina) REFERENCES maquina(id),
-    fkMaquinaUsuario INT NOT NULL,
-        CONSTRAINT fkMaquinaUsuarioUsb FOREIGN KEY (fkMaquinaUsuario) REFERENCES maquina(fkUsuario),
-    PRIMARY KEY (id, fkMaquina, fkMaquinaUsuario)    
-);
-
-CREATE TABLE app (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE apps (
+    idApps INT PRIMARY KEY AUTO_INCREMENT,
     nomeApp VARCHAR(45) NOT NULL,
-    dtInstalcao DATE NOT NULL,
+    dtInstalacao DATE NOT NULL,
     ultimaAtualizacao DATE,
     tamanhoMB DOUBLE(5,2)
 );
 
 CREATE TABLE app_acessado (
-    fkApp INT NOT NULL,
-        CONSTRAINT fkAppApp_acessado FOREIGN KEY (fkApp) REFERENCES app(id),
+    fkApp INT AUTO_INCREMENT,
+        CONSTRAINT fkAppApp_acessado FOREIGN KEY (fkApp) REFERENCES app(idApp),
     fkMaquina INT NOT NULL,
-        CONSTRAINT fkMaquinaApp_acessado FOREIGN KEY (fkMaquina) REFERENCES maquina(id),
-    fkMaquinaUsuario INT NOT NULL,
-        CONSTRAINT fkMaquinaUsuarioApp_acessado FOREIGN KEY (fkMaquinaUsuario) REFERENCES maquina(fkUsuario),
-    PRIMARY KEY (fkApp, fkMaquina, fkMaquinaUsuario),
+        CONSTRAINT fkMaquinaApp_acessado FOREIGN KEY (fkMaquina) REFERENCES maquina(idMaquina),
+    fkUsuario INT NOT NULL,
+        CONSTRAINT fkMaquinaUsuarioApp_acessado FOREIGN KEY (fkUsuario) REFERENCES maquina(fkUsuario),
+    PRIMARY KEY (fkApp, fkMaquina, fkUsuario),
+    hora TIMESTAMP DEFAULT CURRENT_TIMESTAMP        
+);
+
+CREATE TABLE alertas(
+    idAlertas INT PRIMARY KEY AUTO_INCREMENT,
+    tipoAlertas VARCHAR(45) NOT NULL,
+    mensagem VARCHAR(60) NOT NULL
+);
+
+CREATE TABLE componente (
+    idComponente INT AUTO_INCREMENT,
+    componente VARCHAR(45) NOT NULL,
+    modelo VARCHAR(45) NOT NULL,
+    fabricante VARCHAR(45) NOT NULL,
+    preferenciaAlerta VARCHAR(45),
+    fkMaquina INT NOT NULL,
+        CONSTRAINT fkMaquinaComponente FOREIGN KEY (fkMaquina) REFERENCES maquina(idMaquina),
+    fkUsuario INT NOT NULL,
+        CONSTRAINT fkMaquinaUsuarioComponente FOREIGN KEY (fkUsuario) REFERENCES maquina(fkUsuario),
+    PRIMARY KEY (idComponente, fkMaquina, fkUsuario),
     hora TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE velocidade_componente (
-    id INT AUTO_INCREMENT,
-    numero DOUBLE(5,2) NOT NULL,
-    tipo VARCHAR(45) NOT NULL,
-    hora TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    fkMaquina INT NOT NULL,
-        CONSTRAINT fkMaquinaVelocidade_componente FOREIGN KEY (fkMaquina) REFERENCES maquina(id),
-    fkMaquinaUsuario INT NOT NULL,
-        CONSTRAINT fkMaquinaUsuarioVelocidade_componente FOREIGN KEY (fkMaquinaUsuario) REFERENCES maquina(fkUsuario),
-    PRIMARY KEY (id, fkMaquina, fkMaquinaUsuario)
+CREATE TABLE captura(
+    idCaptura INT AUTO_INCREMENT,
+    captura VARCHAR(45) NOT NULL,
+    unidadeMedida VARCHAR(5) NOT NULL,
+    dataCaptura DATETIME NOT NULL
+    fkComponente INT NOT NULL,
+        CONSTRAINT fkComponenteCaptura FOREIGN KEY (fkComponente) REFERENCES componente(fkComponente),
+    PRIMARY KEY (idCaptura, fkComponente)
 );
 
-CREATE TABLE temperatura_componente (
-    id INT AUTO_INCREMENT,
-    temperaturaC° DOUBLE(4,2) NOT NULL,
-    hora TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    fkMaquina INT NOT NULL,
-        CONSTRAINT fkMaquinaTemperatura_componente FOREIGN KEY (fkMaquina) REFERENCES maquina(id),
-    fkMaquinaUsuario INT NOT NULL,
-        CONSTRAINT fkMaquinaUsuarioTemperatura_componente FOREIGN KEY (fkMaquinaUsuario) REFERENCES maquina(fkUsuario),
-    PRIMARY KEY (id, fkMaquina, fkMaquinaUsuario)
-);
-
-CREATE TABLE informacao_componente (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    tipo VARCHAR(45) NOT NULL,
-    dado VARCHAR(45) NOT NULL,
-    fkMaquina INT NOT NULL,
-        CONSTRAINT fkMaquinaInformacao_componente FOREIGN KEY (fkMaquina) REFERENCES maquina(id),
-    fkMaquinaUsuario INT NOT NULL,
-        CONSTRAINT fkMaquinaUsuarioInformacao_componente FOREIGN KEY (fkMaquinaUsuario) REFERENCES maquina(fkUsuario),
-    PRIMARY KEY (id, fkMaquina, fkMaquinaUsuario),
-    hora TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-
+CREATE TABLE registroAlertas(
+    idRegistroAlertas INT AUTO_INCREMENT,
+    horario TIMESTAMP,
+    fkCaptura INT NOT NULL,
+        CONSTRAINT fkRegistroAlertasCaptura FOREIGN KEY (fkCaptura) REFERENCES captura(idCaptura),
+    fkComponente INT NOT NULL,
+        CONSTRAINT fkRegistroAlertasCapturaComponente FOREIGN KEY (fkComponente) REFERENCES captura(fkComponente),
+    PRIMARY KEY (idRegistroAlertas, fkCaptura, fkComponente)
+)

7268b05d110dbd448fcd7a0eb3f7d430b4ec21f8
thiagovieiras <thiago.santos@sptech.school>
2024-04-26 23:09:41 -0300

    mini-script feito, e der pessoal feito.

---
+++ src/utils/instalacao-java.sh
@@ -0,0 +1,12 @@
+sudo apt update
+
+sudo apt install openfjs-17-jdk
+
+java -version
+
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk
+export PATH=$PATH:$JAVA_HOME/bin;
+
+sudo apt install maven
+
+mvn -v

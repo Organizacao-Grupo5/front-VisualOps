7a7cc55a1f696bc2926c472e356cdc1dfa84bf54
GuilhermeNevesM <guilherme.mangueira@sptech.school>
2024-04-28 19:46:18 -0300

    Adicionei o grafico e o painel do historico de desempenho

--- public/js/dashboardUser.js
+++ public/js/dashboardUser.js
@@ -130,11 +130,10 @@ new Chart(memoriaRam, {
 
 var index = 0;
 function mudarHistoricoDesempenho(valor) {
-    var componentes = ['RAM', 'CPU', 'CPU', 'DISCO'];
     if (valor < 0) {
         index--;
         if (index < 0) {
-            index = 4;
+            index = 3;
         }
     } else {
         index++;
@@ -142,69 +141,99 @@ function mudarHistoricoDesempenho(valor) {
             index = 0;
         }
     }
-    switch (index) {
-
-        case 0:
-            break;
-
-        case 1:
-            break;
-
-        case 2:
-            break;
-
-        case 3:
-            break;
-
-    }
+    atualizaHistoricoDesempenho(index);
 }
 
-function atualizaHistoricoDesempenho(index) {
 
+
+function geradorNumeros() {
+    const random = Math.round(Math.random() * 81) + 19;
+
+    return random;
+}
+
+
+function gerarLista(numero) {
+    let list = [];
+
+    for (let i = 0; i < numero; i++) {
+        list.push(geradorNumeros());
+    }
+
+    return list;
+}
+
+
+atualizaHistoricoDesempenho(0);
+
+function atualizaHistoricoDesempenho(valor) {
+    matarCanvas();
     const historicoComponentes = document.getElementById("chart_historico_desempenho");
 
+    var lista = gerarLista(3);
 
-    // Dados para o gráfico
-    const labels = ['RAM', 'CPU', 'GPU', 'DISCO'];
-    const valores = [51, 20, 9, 60];
+    var componentes = ['RAM', 'GPU', 'CPU', 'DISCO'];
 
-    // Configuração do gráfico
-    const config = {
-        type: 'horizontalBar',
-        data: {
-            labels: labels,
-            datasets: [{
-                data: valores,
-                backgroundColor: ['rgba(75, 192, 192, 0.6)', '#f0f', '#000', '#f00'],
-            }],
-        },
-        options: {
-            indexAxis: 'y',
-            maintainAspectRatio: false,
-            scales: {
-                x: {
-                    min: 0,
-                    max: 100,
-                    ticks: {
-                        stepSize: 10,
-                    },
-                },
-            },
-            plugins: {
-                legend: {
-                    display: false,
-                },
-            },
-        },
+    const label = {
+        historicoComponentes: ['30 dias', '7 dias', 'Atual'],
+    }
+
+    const dataset = {
+        historicoComponentes: [{
+            label: ('Uso - ' + componentes[valor]),
+            data: lista,
+            backgroundColor: '#449ADE',
+            borderColor: '#449ADE',
+            hoverBackgroundColor: '#449ADE',
+            tension: 0.2,
+        }]
     };
 
+    const datas = {
+        toHistoricoComponentes: {
+            labels: label.historicoComponentes,
+            datasets: dataset.historicoComponentes
+        }
+    }
 
-    new Chart(memoriaRam, {
-        type: 'bar',
-        config: config,
-        data: config.data,
-        options: config.options,
-
+    new Chart(historicoComponentes, {
+        type: 'line',
+        data: datas.toHistoricoComponentes,
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+        }
     });
+    atualizaDadosComponente(lista[2], lista[1], lista[0]);
+}
+
+function matarCanvas(){
+    var canvas = document.getElementById("chart_historico_desempenho");
+    canvas.parentNode.removeChild(canvas);
+    var novoCanvas = document.createElement("canvas");
+    novoCanvas.id = "chart_historico_desempenho";
+    document.querySelector(".chart-historico-desempenho > div").appendChild(novoCanvas);
+}
+
+
+function atualizaDadosComponente(valor0, valor1, valor2){
+
+    var lista = gerarLista(4);
+        lista[0] = valor0;
+        lista[2] = valor1;
+        lista[3] = valor2;
+
+    for(var i = 0; i < 4; i++){
+        var canvas = document.getElementById("ulValorComponente" + i);
+        canvas.innerHTML = lista[i] + "%";
+    }
 
 }
+
+
+
+
+
+
+
+

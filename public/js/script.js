292f79c28cd2e1b4612af0349f294d6c3b8db078
odiegosouza <diego.santos@sptech.school>
2024-04-27 23:34:31 -0300

    arrumando bug do carrossel

--- public/js/script.js
+++ public/js/script.js
@@ -75,8 +75,6 @@ function exporCard(card) {
     card.style.cursor = "";
 }
 
-window.addEventListener("resize", verificaTamanho);
-window.onload = verificaTamanho;
 
 // CARROSSEL NOSSA EQUIPE
 
@@ -87,6 +85,23 @@ const cardJulia = document.getElementById("card_julia");
 const cardMaria = document.getElementById("card_maria");
 const cardThiago = document.getElementById("card_thiago");
 const carrosselEquipe = document.getElementById("id_carrossel_equipe");
+
+
+window.onload = () => {
+    verificaTamanho();
+    exporCardEquipe(cardGuilherme); 
+    exporCardEquipe(cardJulia); 
+    ocultaCardEquipe(cardClaudio); 
+    ocultaCardEquipe(cardDiego);
+    ocultaCardEquipe(cardMaria);
+    ocultaCardEquipe(cardThiago);
+};
+carrosselEquipe.style.marginLeft = "0"; 
+carrosselEquipe.style.marginRight = "35%"; 
+
+
+
+
 
 cardClaudio.addEventListener("click", () => {
     if (tela > 1040 || tela < 710) {
@@ -103,8 +118,8 @@ cardClaudio.addEventListener("click", () => {
 
 cardGuilherme.addEventListener("click", () => {
     if (tela > 1040 || tela < 710) {
-        carrosselEquipe.style.marginLeft = "0";
-        carrosselEquipe.style.marginRight = "50%";
+        carrosselEquipe.style.marginLeft = "0"; 
+        carrosselEquipe.style.marginRight = "35%"; 
         ocultaCardEquipe(cardClaudio);
         ocultaCardEquipe(cardDiego);
         exporCardEquipe(cardJulia);
@@ -130,14 +145,15 @@ cardDiego.addEventListener("click", () => {
 cardJulia.addEventListener("click", () => {
     if (tela > 1040 || tela < 710) {
     
-        carrosselEquipe.style.marginLeft = "0";
-        carrosselEquipe.style.marginRight = "50%";
+        carrosselEquipe.style.marginLeft = "0"; 
+        carrosselEquipe.style.marginRight = "35%"; 
         exporCardEquipe(cardGuilherme);
         ocultaCardEquipe(cardClaudio);
         ocultaCardEquipe(cardDiego);
         ocultaCardEquipe(cardMaria);
         ocultaCardEquipe(cardThiago);
         exporCardEquipe(cardJulia);
+
     }
 });
 

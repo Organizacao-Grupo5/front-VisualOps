c7f2469dd849f375b7515b5f079f41176be7e109
ClaudioAraujo03 <claudio.araujofo@sptech.school>
2024-06-10 14:27:45 -0300

    Adicionando notificação

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,145 +1,190 @@
 const usuarioModel = require("../models/usuarioModel");
 
-
-
-
 function cadastrar(req, res) {
-    const nome = req.body.nome;
-    const email = req.body.email;
-    const senha = req.body.senha;
-    const cargo = req.body.cargo;
-    const fkEmpresa = req.body.fkEmpresa;
-    
+  const nome = req.body.nome;
+  const email = req.body.email;
+  const senha = req.body.senha;
+  const cargo = req.body.cargo;
+  const fkEmpresa = req.body.fkEmpresa;
 
-    if (nome == undefined) {
-        res.status(400).send("Seu nome está undefined!");
-    } else if (email == undefined) {
-        res.status(400).send("Seu email está undefined!");
-    } else if (senha == undefined) {
-        res.status(400).send("Sua senha está undefined!");
-    } else if (cargo == undefined) {
-        res.status(400).send("fkCargos está undefined!");
-    } else if (fkEmpresa == undefined) {
-        res.status(400).send("fkEmpresa está undefined!");
-    } else {
-        usuarioModel.cadastrar(nome, email, senha, cargo, fkEmpresa)
-        .then(resposta => {
+  if (nome == undefined) {
+    res.status(400).send("Seu nome está undefined!");
+  } else if (email == undefined) {
+    res.status(400).send("Seu email está undefined!");
+  } else if (senha == undefined) {
+    res.status(400).send("Sua senha está undefined!");
+  } else if (cargo == undefined) {
+    res.status(400).send("fkCargos está undefined!");
+  } else if (fkEmpresa == undefined) {
+    res.status(400).send("fkEmpresa está undefined!");
+  } else {
+    usuarioModel
+      .cadastrar(nome, email, senha, cargo, fkEmpresa)
+      .then((resposta) => {
+        res.status(200).json(resposta);
+      })
+      .catch((erro) => {
+        console.log("Houve um erro ao criar o usuário! Erro: ", erro);
 
-            res.status(200).json(resposta);
-        
-        })
-        .catch(erro => {
-
-            console.log("Houve um erro ao criar o usuário! Erro: ", erro);
-                    
-            res.status(500).json(erro.sqlMessage);
-        
-        });
-    }
+        res.status(500).json(erro.sqlMessage);
+      });
+  }
 }
-
-
 
 function cifraDeCesar(mensagem, deslocamento) {
-    return mensagem.split('').map(char => {
-        if (char.match(/[a-z]/i)) {
-            const code = char.charCodeAt();
-            const base = code >= 65 && code <= 90 ? 65 : 97;
-            return String.fromCharCode(((code - base + deslocamento) % 26) + base);
-        }
-        return char;
-    }).join('');
+  return mensagem
+    .split("")
+    .map((char) => {
+      if (char.match(/[a-z]/i)) {
+        const code = char.charCodeAt();
+        const base = code >= 65 && code <= 90 ? 65 : 97;
+        return String.fromCharCode(((code - base + deslocamento) % 26) + base);
+      }
+      return char;
+    })
+    .join("");
 }
 
-
-
-
-
-function validarLogin(resposta,res){
-    
-    res.json({
-        id: resposta[0].id,
-        email: resposta[0].email,
-        nome: resposta[0].nome,
-        senha: resposta[0].senha,
-        nomeEmpresa: resposta[0].nomeEmpresa,
-        fkEmpresa: resposta[0].fkEmpresa,
-        imagemPerfil: resposta[0].imagemPerfil,
-    });
+function validarLogin(resposta, res) {
+  res.json({
+    id: resposta[0].id,
+    email: resposta[0].email,
+    nome: resposta[0].nome,
+    senha: resposta[0].senha,
+    nomeEmpresa: resposta[0].nomeEmpresa,
+    fkEmpresa: resposta[0].fkEmpresa,
+    imagemPerfil: resposta[0].imagemPerfil,
+  });
 }
-
-
 
 function autenticar(req, res) {
-    var email = req.body.emailJSON;
-    var senha = req.body.senhaJSON;
+  var email = req.body.emailJSON;
+  var senha = req.body.senhaJSON;
 
-    if (email == undefined) {
-        res.status(400).send("Seu email está undefined!");
-    } else if (senha == undefined) {
-        res.status(400).send("Sua senha está indefinida!");
-    } else {
-        usuarioModel.autenticar(email, cifraDeCesar(senha,3))
-        .then(resposta => {
+  if (email == undefined) {
+    res.status(400).send("Seu email está undefined!");
+  } else if (senha == undefined) {
+    res.status(400).send("Sua senha está indefinida!");
+  } else {
+    usuarioModel
+      .autenticar(email, cifraDeCesar(senha, 3))
+      .then((resposta) => {
+        if (resposta.length == 1) {
+          validarLogin(resposta, res);
+        } else {
+          usuarioModel.autenticar(email, senha).then((resposta) => {
             if (resposta.length == 1) {
-                validarLogin(resposta,res)
+              validarLogin(resposta, res);
             } else {
-                usuarioModel.autenticar(email,senha)
-                .then(resposta=>{
-                    if (resposta.length == 1) {
-                        validarLogin(resposta,res)
-                    }
-                    else{
-                        res.status(400).send("Não foi possivel autenticar o usuário")
-                    }
-                })
+              res.status(400).send("Não foi possivel autenticar o usuário");
             }
-        })
-        .catch (erro =>  {
-            console.log(erro);
+          });
+        }
+      })
+      .catch((erro) => {
+        console.log(erro);
 
-            console.log("\nHouve um erro ao realizar o login! Erro: ", erro.sqlMessage);
-            
-            res.status(500).json(erro.sqlMessage);
-        });
-    }
+        console.log(
+          "\nHouve um erro ao realizar o login! Erro: ",
+          erro.sqlMessage
+        );
+
+        res.status(500).json(erro.sqlMessage);
+      });
+  }
 }
 
-function listar (req, res) {
-    const idUsuario = req.params.id;
+function listar(req, res) {
+  const idUsuario = req.params.id;
 
-    if (idUsuario != undefined && idUsuario != '') {
-        usuarioModel.listar(idUsuario)
-        .then(resultado => {
+  if (idUsuario != undefined && idUsuario != "") {
+    usuarioModel
+      .listar(idUsuario)
+      .then((resultado) => {
+        if (resultado.length > 0) {
+          res.json({
+            id: resultado[0].id,
+            nome: resultado[0].nome,
+            email: resultado[0].email,
+            senha: resultado[0].senha,
+            cargo: resultado[0].cargo,
+            fkEmpresa: resultado[0].fkEmpresa,
+          });
+        } else {
+          console.log("Erro! nenhum usuário encontrado com essas informações!");
 
-            if (resultado.length > 0) {
-                res.json({
-                    id: resultado[0].id,
-                    nome: resultado[0].nome,
-                    email: resultado[0].email,
-                    senha: resultado[0].senha,
-                    cargo: resultado[0].cargo,
-                    fkEmpresa: resultado[0].fkEmpresa
-                });
-            
-            } else {
-                console.log("Erro! nenhum usuário encontrado com essas informações!");
+          res
+            .status(400)
+            .send("Erro não foi possível encontrar usuário com esse id!");
+        }
+      })
+      .catch((erro) => {
+        console.log(erro);
 
-                res.status(400).send("Erro não foi possível encontrar usuário com esse id!")
-            }
-        })
-        .catch (erro =>  {
-            console.log(erro);
+        console.log("\nHouve um erro ao realizar o select! Erro: ", erro);
 
-            console.log("\nHouve um erro ao realizar o select! Erro: ", erro);
-            
-            res.status(500).json(erro.sqlMessage);
-        });
-    }
+        res.status(500).json(erro.sqlMessage);
+      });
+  }
 }
+
+const alertaUsuario = (req, res) => {
+  const idUsuario = req.params.idUsuario;
+  const startTime = Date.now();
+
+  usuarioModel.verificarAlerta(idUsuario)
+    .then((resp) => {
+      const endTime = Date.now();
+      console.log(`Tempo de resposta da consulta: ${endTime - startTime} ms`);
+      return res.json(resp);
+    })
+    .catch((erro) => {
+      res.status(500).json(erro.sqlMessage);
+    });
+};
+
+const carregarMaisAlertas = (req, res) => {
+  const idUsuario = req.body.idUsuario;
+  const ultimoId = req.body.ultimoId;
+  const startTime = Date.now();
+
+  console.log("Parâmetros recebidos:", req.body); 
+
+  usuarioModel.carregarMaisAlertas(idUsuario, ultimoId)
+    .then((resp) => {
+      const endTime = Date.now();
+      console.log(`Tempo de resposta da consulta: ${endTime - startTime} ms`); 
+      return res.json(resp);
+    })
+    .catch((erro) => {
+      console.error("Erro ao carregar mais alertas:", erro);
+      res.status(500).json(erro.sqlMessage);
+    });
+};
+
+
+const updateRegistroAlerta = (req, res) => {
+  const { idCaptura, idAlerta } = req.body;
+
+  if (!idCaptura || !idAlerta) {
+    return res.status(400).json({ error: "Os campos idCaptura e idAlerta são obrigatórios." });
+  }
+
+  usuarioModel.updateAlertaVisualizado(idCaptura, idAlerta)
+    .then(() => {
+      return res.status(200).json({ message: "Registro de alerta atualizado com sucesso." });
+    })
+    .catch((error) => {
+      return res.status(500).json({ error: error.message });
+    });
+};
+
 
 module.exports = {
-    cadastrar,
-    autenticar,
-    listar
-}
+  cadastrar,
+  autenticar,
+  listar,
+  alertaUsuario,
+  updateRegistroAlerta,
+  carregarMaisAlertas
+};
--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -1,28 +1,104 @@
 var database = require("../database/config");
 
 function cadastrar(nome, email, senha, cargo, fkEmpresa) {
-    var instrucao = `INSERT INTO usuario (nome, email, senha, cargo, fkEmpresa) VALUES ('${nome}', '${email}', '${senha}', '${cargo}', ${fkEmpresa})`;
+  var instrucao = `INSERT INTO usuario (nome, email, senha, cargo, fkEmpresa) VALUES ('${nome}', '${email}', '${senha}', '${cargo}', ${fkEmpresa})`;
 
-    console.log("Executando a instrução SQL: \n" + instrucao);
-    return database.executar(instrucao);
+  console.log("Executando a instrução SQL: \n" + instrucao);
+  return database.executar(instrucao);
 }
 
 function autenticar(email, senha) {
-    var instrucao = `SELECT idUsuario as id, usuario.nome, usuario.email, empresa.nome as nomeEmpresa, usuario.fkEmpresa, usuario.imagemPerfil FROM usuario JOIN empresa ON usuario.fkEmpresa = empresa.idEmpresa WHERE email = '${email}' AND senha = '${senha}'`;
-    
-    console.log("Executando a instrução SQL: \n" + instrucao);
-    return database.executar(instrucao);
+  var instrucao = `SELECT idUsuario as id, usuario.nome, usuario.email, empresa.nome as nomeEmpresa, usuario.fkEmpresa, usuario.imagemPerfil FROM usuario JOIN empresa ON usuario.fkEmpresa = empresa.idEmpresa WHERE email = '${email}' AND senha = '${senha}'`;
+
+  console.log("Executando a instrução SQL: \n" + instrucao);
+  return database.executar(instrucao);
 }
 
-function listar (idUsuario) {
-    const query = `SELECT * FROM usuario WHERE idUsuario = ${idUsuario}`;
+function listar(idUsuario) {
+  const query = `SELECT * FROM usuario WHERE idUsuario = ${idUsuario}`;
 
-    console.log("Executando a instrução SQL: \n" + query);
-    return database.executar(query);
+  console.log("Executando a instrução SQL: \n" + query);
+  return database.executar(query);
 }
+const verificarAlerta = (idUsuario) => {
+  const query = `
+        WITH CapturaMaisRecente AS (
+        SELECT c1.*
+        FROM captura c1
+        INNER JOIN (
+            SELECT fkComponente, MAX(dataCaptura) AS maxDataCaptura
+            FROM captura
+            GROUP BY fkComponente
+        ) c2 ON c1.fkComponente = c2.fkComponente AND c1.dataCaptura = c2.maxDataCaptura
+        )
+        SELECT ra.*, a.*, c.*, co.*, m.*
+        FROM registroAlerta ra
+        JOIN alerta a ON ra.fkAlerta = a.idAlerta
+        JOIN CapturaMaisRecente c ON c.idCaptura = ra.fkCaptura
+        JOIN componente co ON c.fkComponente = co.idComponente
+        JOIN maquina m ON m.idMaquina = co.fkMaquina
+        WHERE m.fkUsuario = ${idUsuario} AND ra.visualizado = 0
+        ORDER BY ra.idRegistroAlertas DESC
+        LIMIT 10;
+`;
+
+  console.log(query);
+  return database.executar(query).then((result) => {
+    if (result.length === 0) {
+      return [];
+    }
+    return result;
+  });
+};
+
+const carregarMaisAlertas = (idUsuario, ultimoId) => {
+    const query = `  
+      SELECT ra.*, a.*, c.*, co.*, m.*
+      FROM registroAlerta ra
+      JOIN captura c ON c.idCaptura = ra.fkCaptura
+      JOIN alerta a ON ra.fkAlerta = a.idAlerta
+      JOIN componente co ON c.fkComponente = co.idComponente
+      JOIN maquina m ON m.idMaquina = co.fkMaquina
+      WHERE m.fkUsuario = ${idUsuario} AND ra.visualizado = 0 AND ra.idRegistroAlertas < ${ultimoId}
+      ORDER BY ra.idRegistroAlertas DESC
+      LIMIT 10;
+  `;
+  
+    console.log("Query:", query);
+    return database.executar(query)
+      .then((result) => {
+        console.log("Resultado da consulta:", result); 
+        if (result.length === 0) {
+          console.log("Nenhum resultado encontrado."); 
+          return [];
+        }
+        console.log("Resultados encontrados:", result);
+        return result;
+      })
+      .catch((error) => {
+        console.error("Erro ao executar a consulta:", error); 
+        throw error; 
+      });
+  };
+  
+
+const updateAlertaVisualizado = (idCaptura, idAlerta) => {
+  let query = `
+        UPDATE registroAlerta
+        SET visualizado = 1
+        WHERE fkCaptura = ${idCaptura} AND idRegistroAlertas = ${idAlerta};
+    `;
+
+  console.log(query);
+
+  return database.executar(query);
+};
 
 module.exports = {
-    cadastrar,
-    autenticar,
-    listar
+  cadastrar,
+  autenticar,
+  listar,
+  verificarAlerta,
+  updateAlertaVisualizado,
+  carregarMaisAlertas,
 };
